4f408f2559fa3252ca608c966311604d882ec5ec
Edward J Kim <edward.junhyung.kim@gmail.com>
2016-08-26 04:22:10 +0000

    use actual student list to test table creation

--- tests/test_assign.py
+++ tests/test_assign.py
@@ -1,12 +1,24 @@
-from pgrader.assign import convert_single
-from pgrader.names_generator import get_random_name
-from pgrader.file import get_users
+import sys
 from nose.tools import assert_equal
 
+from pgrader.assign import make_table
+from pgrader.names_generator import get_random_name
 
-def test_convert_single():
 
-    users = get_users("tests/users.yml")
-    names = [convert_single(user, 1) for user in users]
-    assert_equal(len(names), len(users))
-        
+def test_create_table():
+
+    def do_weeks_from_student_list(filename, nweeks=15):
+
+        with open(filename) as f:
+            users = [line.strip().split()[0] for line in f]
+    
+        for week in range(nweeks):
+            names = make_table(users, week)
+            assert_equal(len(names.keys()), len(users))
+            assert_equal(set(names.keys()), set(users))
+            assert_equal(len(set(names.values())), len(users))
+
+        sys.stdout.write('{} users.'.format(len(users)))
+
+    do_weeks_from_student_list('tests/studentlist.accy')
+    do_weeks_from_student_list('tests/studentlist.info490')
