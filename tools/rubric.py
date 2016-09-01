05fa514cfc008280dd4cb3fc8a65f9a8c5a1fd03
Edward J Kim <edward.junhyung.kim@gmail.com>
2016-09-01 00:55:41 +0000

    update rubric

--- tools/rubric.py
+++ tools/rubric.py
@@ -5,11 +5,8 @@ from IPython.display import display, Javascript
 
 class Rubric():
     
-    def __init__(self, nb_name, yml_path=None, nproblems=3, **kwargs):
-        
+    def __init__(self, nb_name, yml_path=None, **kwargs):
         """Constructor"""
-        
-        self.nproblems = nproblems
 
         week, assignment = nb_name.split('/')
         assignment = assignment.split('.')[0]
@@ -22,34 +19,44 @@ class Rubric():
         assignments = [f.split('.')[0] for f in os.listdir(os.getcwd()) if f.endswith('.ipynb')]
         self.all_problems = list(set(['_'.join(a.split('_')[:3]) for a in assignments]))
         self.all_students = ['_'.join(a.split('_')[-3:]) for a in assignments]
-        
+
         if yml_path is None:
             self.yml_path = os.path.join('{}.yml'.format(os.getcwd().split('/')[-1]))
 
-            
-    def getname(self):
-        display(Javascript('''
-            var nb = IPython.notebook;
-            var kernel = IPython.notebook.kernel;
-            var command = "NOTEBOOK_FULL_PATH = '" + nb.base_url + nb.notebook_path + "'";
-            kernel.execute(command);
-            '''
-        ))
-        return NOTEBOOK_FULL_PATH
 
-    
     def _display_table(self, table, title):
-        
+
         for idx, row in enumerate(table):
             if idx < 1:
                 left = title
             else:
                 left = ' ' * len(title)
-                
+
             print('{} | {} points | {}'.format(left, row, table[row]))
+
         print()
-            
-        
+
+
+    def _get_input(self, name, minimum=0, maximum=5):
+
+        value = input("Enter your score for {}: ".format(name))
+
+        while True:
+            try:
+                value = float(value)
+                if value >= minimum and value <= maximum:
+                    break
+            except:
+                pass
+
+            value = input(
+                "Your score must be between 0 and 5. "
+                "Enter your score for {}: ".format(name)
+            )
+
+        return value
+
+
     def display_rubric(self):
 
         self.correctness = {
@@ -58,34 +65,33 @@ class Rubric():
             4: 'Code runs, produces correct output but output is difficult to understand.',
             5: 'Code runs, produces correct output and output is easy to understand.'
         }
-        
+
         self.readability = {
             0: 'Code is not documented and is impossible to understand.',
             2: 'Code is poorly documented, and uses non-recommended practices.',
             4: 'Code is documented, but uses non-recommended practices',
             5: 'Code is fully documented (as appropriate), and uses good programming practices.'
         }
-        
+
         print('Assessment Form\n')
         print('-' * 80)
-        self._display_table(self.correctness, 'Correctness')
-        self.correctness_value = eval(input('Enter your score for correctness: '))
-        
-        print('-' * 80)
-        self._display_table(self.readability, 'Readability')
-        self.readability_value = eval(input('Enter your score for readability: '))
 
+        self._display_table(self.correctness, 'Correctness')
+        self.correctness_value = self._get_input('correctness')
         print('-' * 80)
-        
+
+        self._display_table(self.readability, 'Readability')
+        self.readability_value = self._get_input('readability')
+        print('-' * 80)
+
         self.comments_value = input('Enter any comments: ')
-        
         print('-' * 80)
-        print('Assessment submitted')
-        print('To change your assessment, simply run the code cell again.')
-        
+
+        print('Assessment saved. To change your assessment, simply run the code cell again.')
+
         self._save_submitted(self.yml_path)
-        self._on_validate_clicked()
-        
+        self._display_tasks()
+
     def _save_submitted(self, path):
 
         s = self.student
@@ -110,11 +116,7 @@ class Rubric():
         with open(path, 'w') as f:
             f.write(yaml.dump(data))
 
-    def _on_validate_clicked(self):
-
-        if not os.path.exists(self.yml_path):
-            print("No data found. You haven't graded any peers.")
-            return
+    def _display_tasks(self):
 
         with open(self.yml_path) as f:
             data = yaml.load(f)
@@ -134,13 +136,19 @@ class Rubric():
                     done[student].append(problem)
                 else:
                     todo[student].append(problem)
-     
-        print('\nSubmitted')
-        print('---------')
+
+        print('\nCompleted tasks')
+        print('-' * 80)
         for s in sorted(done):
             print('{}: {}'.format(s, ', '.join(done[s])))
-        print('')
+        print('' * 80)
         print('Remaining tasks')
         print('---------------')
-        for s in sorted(todo):
-            print('{}: {}'.format(s, ', '.join(todo[s])))
+
+        # check for any non-empty list, an empty list is False
+        if any(todo.values()):
+            for s in sorted(todo):
+                print('{}: {}'.format(s, ', '.join(todo[s])))
+        # if all empty
+        else:
+            print('You have no remaining tasks.\n\nYou are ready to submit. Go to Assignments tab and click Submit.')
