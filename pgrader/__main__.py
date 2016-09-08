883edb334115d211ead8b7ae53b512166d067614
Edward J Kim <edward.junhyung.kim@gmail.com>
2016-09-08 14:33:43 +0000

    update the main file

--- pgrader/__main__.py
+++ pgrader/__main__.py
@@ -1,6 +1,8 @@
 import sys
 from pgrader.assign import get_users, assign_notebooks
 from pgrader.fetch import fetch_notebooks
+from pgrader.autograde import get_peer_grading, get_peer_assessment
+from pgrader.comments import get_comments
 
 
 def main(args=None):
@@ -15,16 +17,55 @@ def main(args=None):
     users = get_users("studentlist.accy")
 
     if args[0] == "fetch":
+        # fetch subcommand requires assignment_id
+        if len(args[1:]) != 1:
+            sys.stderr.write("Usage: pgrader fetch <assignment_id>\n")
+            return 1
+        assignment_id = args[1]
         for user in users:
             fetch_notebooks(
                 "/home/ubuntu/export/exchange",
                 "/home/ubuntu/submitted",
-                user, "accy", "accy570_hw1_due_8_30"
+                user, "accy", assignment_id
             )
+
     elif args[0] == "assign":
-        assign_notebooks(users, "accy570_hw1_due_8_30", 1, remove_header=True)
+        # assign subcommand requires assignment_id and week_number
+        if len(args[1:]) != 2:
+            sys.stderr.write("Usage: pgrader assign <assignment_id> <week_number>\n")
+            return 1
+        assignment_id = args[1]
+        week_number = args[2]
+        assign_notebooks(users, assignment_id, week_number, remove_header=True)
+
+    elif args[0] == "given":
+        # autograde subcommand requires assignment_id
+        if len(args[1:]) != 1:
+            sys.stderr.write("Usage: pgrader given <assignment_id>\n")
+            return 1
+        assignment_id = args[1]
+        get_peer_grading(users, assignment_id)
+
+    elif args[0] == "received":
+        # autograde subcommand requires assignment_id and week_number
+        if len(args[1:]) != 2:
+            sys.stderr.write("Usage: pgrader received <assignment_id> <week_number>\n")
+            return 1
+        assignment_id = args[1]
+        week_number = args[2]
+        get_peer_assessment(users, assignment_id, week_number)
+
+    elif args[0] == "comments":
+        # autograde subcommand requires assignment_id and week_number
+        if len(args[1:]) != 2:
+            sys.stderr.write("Usage: pgrader comments <assignment_id> <week_number>\n")
+            return 1
+        assignment_id = args[1]
+        week_number = args[2]
+        get_comments(users, assignment_id, week_number)
+
     else:
-        sys.stderr.write("Valid subcommands are: fetch, assign\n")
+        sys.stderr.write("Valid subcommands are: fetch, assign, given, received\n")
 
 
 if __name__ == "__main__":
